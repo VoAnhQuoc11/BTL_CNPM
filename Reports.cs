84e562a78c7446c173ddfe8302fa8c0f3b553674
kha8805 <khanh0328873909@gmai.com>
2024-10-20 21:52:00 +0700

    Test

---
+++ Reports.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bài_tập_C_
+{
+    internal class Reports
+    {
+        
+            // Properties
+            public int ReportID { get; set; }
+            public string ReportType { get; set; }
+            public string CreatedDate { get; set; }
+
+            // Constructor
+            public Reports(int id, string type, string date)
+            {
+                ReportID = id;
+                ReportType = type;
+                CreatedDate = date;
+            }
+
+            // Methods
+            public string GenerateKoiGrowthReport(int KoiFishID)
+            {
+                // Logic to generate Koi Growth Report
+                return $"Koi Growth Report for KoiFish ID: {KoiFishID}";
+            }
+
+            public string GenerateWaterQualityReport(int PondID)
+            {
+                // Logic to generate Water Quality Report
+                return $"Water Quality Report for Pond ID: {PondID}";
+            }
+
+            public string GenerateSalesReport(string DateRange)
+            {
+                // Logic to generate Sales Report
+                return $"Sales Report for Date Range: {DateRange}";
+            }
+        }
+    }
+
