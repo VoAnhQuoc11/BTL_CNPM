a467c420ce456bd2c4f23b448565d1c6fe1984d6
HongTrungKien <kienht8890@ut.edu.vn>
2024-11-03 20:28:46 +0700

    Add files via upload

---
+++ KoiFishApp.Services/Interface/IPondServices.cs
@@ -0,0 +1,14 @@
+﻿using KoiFishApp.Repositories.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiFishApp.Services.Interfaces
+{
+    public interface IPondServices
+    {
+        Task<List<Pond>> GetAllPondsAsync();
+    }
+}
