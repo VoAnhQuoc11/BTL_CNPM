7631507e6cfb5781433ff40f66279fb84dd35015
TN-Kieran <trognghia284@gmail.com>
2024-11-12 01:49:20 +0700

    Upload IProduct.cs

---
+++ KoiFishApp.Repositories/Interface/IProductsRepository.cs
@@ -0,0 +1,15 @@
+﻿using KoiCare.Repositories.Enteteis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiCare.Repositories.Interfaces
+{
+    public interface IProducts
+    {
+        Task<List<Product>> GetProducts();
+        Task<List<GioHang>> GetGioHangs();
+    }
+}
