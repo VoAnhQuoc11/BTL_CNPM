b126b4019a3adea9ec9153a78d6546fa12d492ac
TN-Kieran <trognghia284@gmail.com>
2024-11-14 00:58:54 +0700

    Upload ProducRepos

---
+++ KoiFishApp.Repositories/Repositories/ProductsRepository.cs
@@ -0,0 +1,34 @@
+﻿using KoiCare.Repositories.Enteteis;
+using KoiCare.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiCare.Repositories.Repositories
+{
+    public class ProductsRepository : IProducts
+    {
+
+        private readonly QlcktnContext _dbcontext;
+        public ProductsRepository(QlcktnContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+        public async Task<List<Product>> GetProducts()
+        {
+            List<Product> products = await _dbcontext.Products.ToListAsync();
+            return products;
+        }
+
+        public async Task<List<GioHang>> GetGioHangs()
+        {
+            List<GioHang> products = await _dbcontext.GioHangs.ToListAsync();
+            
+            return products;
+        }
+
+    }
+}
