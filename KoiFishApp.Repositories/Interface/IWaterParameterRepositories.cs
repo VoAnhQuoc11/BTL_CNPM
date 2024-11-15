d4fc7a8187ff43e7f397b42af8683d9ed68e1331
ThinhNguyen2005 <nguyenthinhk52005@gmail.com>
2024-11-15 21:50:52 +0700

    Add files via upload

---
+++ KoiFishApp.Repositories/Interface/IWaterParameterRepositories.cs
@@ -0,0 +1,33 @@
+﻿using KoiFishApp.Repositories.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiFishApp.Repositories.Interfaces
+{
+    public interface IWaterParameterRepositories
+    {
+        //Get all water parameters
+        Task<List<WaterParameter>> GetAllWaterParametersAsync();
+
+        //Get water parameter by ID
+       Task<WaterParameter> GetWaterParameterByIdAsync(int id);
+
+        // Get water parameters by pond ID
+        Task<List<WaterParameter>> GetWaterParametersByPondIdAsync(int pondId);
+
+        //Add new water parameter
+        Task AddWaterParameterAsync(WaterParameter waterParameter);
+
+        // Update existing water parameter
+        Task UpdateWaterParameterAsync(WaterParameter waterParameter);
+
+        // Delete water parameter by ID
+        Task DeleteWaterParameterAsync(int id);
+
+        // Add default water parameter for a pond
+        Task AddWaterParameterForPondIDAsync(int pondId);
+    }
+}
