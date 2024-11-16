cf1a094ef75cf82506258650aed71971b3517531
dichvietduc2005 <dvd192005@gmail.com>
2024-11-16 21:19:02 +0700

    Update IUserRepositories.cs

--- KoiFishApp.Repositories/Interface/IUserRepositories.cs
+++ KoiFishApp.Repositories/Interface/IUserRepositories.cs
@@ -1,4 +1,6 @@
-﻿using KoiFishApp.Repositories.Entities;
+
+using KoiFishApp.Repositories.Entities;
+using KoiFishApp.Repositories.Intrefaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +12,6 @@ namespace KoiFishApp.Repositories.Intrefaces
     public interface IUserRepositories
     {
         Task<User> GetUserByIdAsync(string userId);
-
         Task<bool> UpdateFullNameAsync(string userId, string fullName);
         Task<bool> UpdateEmailAsync(string userId, string email);
         Task<bool> UpdatePhoneAsync(string userId, string phone);
@@ -18,9 +19,20 @@ namespace KoiFishApp.Repositories.Intrefaces
         Task<bool> UpdateImageAsync(string userId, string imageUrl);
         Task<bool> UpdateBirthDateAsync(string userId, DateOnly birthDate);
         Task<bool> UpdateGenderAsync(string userId, bool gender);
+        Task<bool> DelUser(String userId);
+        Task<bool> DelUser(User user);
+        Task<bool> AddUser(User user);
+        Task<bool> UpUser(User user);
+        Task<List<User>> GetAllUser();
 
-
+        User CreateUser(string Id,string username,string fullname,string password,string imageUrl,bool gender, DateOnly birthDate, string phone, bool status, string email);
 
         User? GetUserByUsernameAndPassword(string username, string password);
     }
 }
+
+
+
+
+
+
