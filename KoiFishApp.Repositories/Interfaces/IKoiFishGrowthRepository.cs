7c1cf10c10360e066f39e02fff67e3501ba10e17
Dương Lê Quốc Khánh <khanh0328873909@gmail.com>
2024-11-16 21:37:34 +0700

    Add files via upload

---
+++ KoiFishApp.Repositories/Interfaces/IKoiFishGrowthRepository.cs
@@ -0,0 +1,17 @@
+﻿using KoiFishApp.Repositories.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiFishApp.Repositories.Interfaces
+{
+    public interface IKoiFishGrowthRepository
+    {
+        Task<List<KoiGrowth>> GetGrowthRecordsByKoiFishIdAsync(int koiId);
+        Task AddGrowthRecordAsync(KoiGrowth record);
+        Task UpdateGrowthRecordAsync(KoiGrowth record);
+        Task DeleteGrowthRecordAsync(int recordId);
+    }
+}
