7aa301712ed6760426c7cee057c5f5751c40d73a
dichvietduc2005 <dvd192005@gmail.com>
2024-11-16 21:33:08 +0700

    Add files via upload

---
+++ KoiFishApp.WebApplication/Pages/Login/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KoiFishApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Trang sẽ tự động chuyển hướng sang trang Login từ Index.cshtml.
+        }
+    }
+}
---
+++ KoiFishApp.WebApplication/Pages/Login/Login.cshtml.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using KoiFishApp.Repositories.Entities;
+using KoiFishApp.Sevices.Interfaces;
+using KoiFishApp.WebApplication.Pages.Home;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+
+namespace KoiFishApp.Pages
+{
+    public class LoginModel : PageModel
+    {
+        private readonly IUserServices _services;
+
+        [BindProperty]
+        public string Username { get; set; }
+
+        [BindProperty]
+        public string Password { get; set; }
+
+        public string? ErrorMessage { get; set; }
+
+        public LoginModel(IUserServices services)
+        {
+            _services = services;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = _services.GetUserByUsernameAndPassword(Username, Password);
+            if (user == null)
+            {
+                ErrorMessage = "Invalid username or password.";
+                return Page();
+            }
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Id), // Sử dụng user.Id để xác định người dùng
+                new Claim(ClaimTypes.NameIdentifier, user.Username)
+            };
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            // Đăng nhập người dùng bằng cookie
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+            return RedirectToPage("/Menu");
+        }
+    }
+}
+
