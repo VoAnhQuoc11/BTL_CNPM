2c91c37fcaa95da7302aaca224ca1a4c4c37bd80
dichvietduc2005 <dvd192005@gmail.com>
2024-11-16 21:34:06 +0700

    Add files via upload

---
+++ KoiFishApp.WebApplication/Pages/Home/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KoiFishApp.WebApplication.Pages.Home
+{
+	public class IndexModel : PageModel
+	{
+		public void OnGet()
+		{
+		}
+	}
+}
