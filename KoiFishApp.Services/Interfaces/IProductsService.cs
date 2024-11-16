d504e3093fc4f2235ec00460f30b5db595910d83
TN-Kieran <trognghia284@gmail.com>
2024-11-17 04:56:36 +0700

    Upload new IProductsService.cs

---
+++ KoiFishApp.Services/Interfaces/IProductsService.cs
@@ -0,0 +1,27 @@
+﻿using KoiFishApp.Repositories.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiFishApp.Services.Interfaces
+{
+    public interface IProductsService
+    {
+        Task<List<Product>> GetCurrentListItems();
+        Task<List<GioHang>> GetGioHangHienTai();
+
+        bool AddProduct(Product product);
+
+        bool AddGioHang(GioHang product);
+
+        bool RemoveProduct(int productId);
+
+        bool RemoveGioHang(int productId);
+
+        Task<Product> GetProductById(int productId);
+
+        Task<GioHang> FindProductInGioHangById(int productId);
+    }
+}
