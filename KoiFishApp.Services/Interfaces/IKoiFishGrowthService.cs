202a7eba382dd0d99e18ccfce7d6204cdf813a61
HongTrungKien <kienht8890@ut.edu.vn>
2024-11-17 12:33:12 +0700

    Update IKoiFishGrowthService.cs

--- KoiFishApp.Services/Interfaces/IKoiFishGrowthService.cs
+++ KoiFishApp.Services/Interfaces/IKoiFishGrowthService.cs
@@ -10,8 +10,10 @@ namespace KoiFishApp.Services.Interfaces
     public interface IKoiFishGrowthService
     {
         Task<List<KoiGrowth>> GetGrowthRecordsByKoiFishIdAsync(int koiId);
+        Task<KoiGrowth> GetGrowthRecordByIdAsync(int koiGrowthId);
         Task AddGrowthRecordAsync(KoiGrowth record);
         Task UpdateGrowthRecordAsync(KoiGrowth record);
         Task DeleteGrowthRecordAsync(int recordId);
+        decimal CalculateFoodAmount(decimal weight, int age);
     }
 }
