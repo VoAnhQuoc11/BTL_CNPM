c261600ced8082228958faab3c9dd33062e260da
VoAnhQuoc11 <quocvogiarai0808@gmail.com>
2024-11-17 13:27:12 +0700

    Cập nhật file mới

---
+++ KoiFishApp.Repositories/Entities/GioHang.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KoiFishApp.Repositories.Entities;
+
+public partial class GioHang
+{
+    public int ProductId { get; set; }
+
+    public string? Name { get; set; }
+
+    public decimal? Price { get; set; }
+
+    public int? Quantity { get; set; }
+
+    public string? Description { get; set; }
+}
---
+++ KoiFishApp.WebApplication/Pages/Menu.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KoiFishApp.WebApplication.Pages
+{
+    public class MenuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
