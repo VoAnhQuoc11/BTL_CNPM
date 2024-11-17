01c368e11cb13e9da909b4abfae07659944a8947
HongTrungKien <kienht8890@ut.edu.vn>
2024-11-17 12:43:31 +0700

    Update Index.cshtml.cs

--- KoiFishApp.WebApplication/Pages/CTKoiFish/Index.cshtml.cs
+++ KoiFishApp.WebApplication/Pages/CTKoiFish/Index.cshtml.cs
@@ -21,16 +21,11 @@ namespace KoiFishApp.WebApplication.Pages.CTKoiFish
         }
 
         public IList<KoiFish> KoiFish { get; set; } = default!;
-        public Dictionary<int, decimal> FoodAmounts { get; set; } = new Dictionary<int, decimal>(); // Khai báo FoodAmounts
+        
         public async Task OnGetAsync()
         {
             KoiFish = await _services.KoiFish();
-            // Tính toán lượng thức ăn cho mỗi cá Koi
-            foreach (var koi in KoiFish)
-            {
-                var foodAmount = await _services.CalculateFoodAmount(koi.KoiId);
-                FoodAmounts[koi.KoiId] = foodAmount;  // Lưu kết quả vào dictionary
-            }
+            
         }
     }
 }
