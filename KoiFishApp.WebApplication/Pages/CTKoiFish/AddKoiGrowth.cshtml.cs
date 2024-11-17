175867baf27da20a9c5ca73a90670aa3ef538322
HongTrungKien <kienht8890@ut.edu.vn>
2024-11-17 12:51:40 +0700

    Add files via upload

---
+++ KoiFishApp.WebApplication/Pages/CTKoiFish/AddKoiGrowth.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using KoiFishApp.Repositories.Entities;
+using KoiFishApp.Services.Interfaces;
+
+namespace KoiFishApp.WebApplication.Pages.CTKoiFish
+{
+    public class AddKoiGrowthModel : PageModel
+    {
+        private readonly IKoiFishGrowthService _growthRecordService;
+        private readonly IKoiFishServices _koiFishService;
+
+        public AddKoiGrowthModel(IKoiFishGrowthService growthRecordService, IKoiFishServices koiFishService)
+        {
+            _growthRecordService = growthRecordService;
+            _koiFishService = koiFishService;
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            // Sử dụng IKoiFishServices để lấy danh sách KoiFish
+            var koiFishList = await _koiFishService.KoiFish();
+            ViewData["KoiId"] = new SelectList(koiFishList, "KoiId", "KoiId");
+            return Page();
+        }
+
+        [BindProperty]
+        public KoiGrowth KoiGrowth { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            await _growthRecordService.AddGrowthRecordAsync(KoiGrowth);
+            return RedirectToPage("/CTKoiFish/Details", new { id = KoiGrowth.KoiId });
+        }
+    }
+}
---
+++ KoiFishApp.WebApplication/Pages/CTKoiFish/EditGrowthRecord.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using KoiFishApp.Repositories.Entities;
+using KoiFishApp.Services.Interfaces;
+using Microsoft.CodeAnalysis.Emit;
+using KoiFishApp.Services.Services;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace KoiFishApp.WebApplication.Pages.CTKoiFish
+{
+    public class EditGrowthRecordModel : PageModel
+    {
+        private readonly IKoiFishGrowthService _growthRecordService;
+        public KoiGrowth _temp { get; set; }
+        public EditGrowthRecordModel(IKoiFishGrowthService growthRecordService)
+        {
+            _growthRecordService = growthRecordService;
+
+        }
+
+        // public int ID { get; set; }
+        [BindProperty]
+        public KoiGrowth KoiGrowth { get; set; } = default!;
+        public async Task<IActionResult> OnGetAsync(int koiGrowthId)
+        {
+            // Lấy thông tin bản ghi tăng trưởng
+            KoiGrowth = await _growthRecordService.GetGrowthRecordByIdAsync(koiGrowthId);
+
+            if (KoiGrowth == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+        public async Task<IActionResult> OnPostAsync(int koiGrowthId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            await _growthRecordService.UpdateGrowthRecordAsync(KoiGrowth);
+            return Page();
+        }
+
+    }
+}
